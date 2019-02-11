dca0700210ca6672787217b89f8183e00acca938
ferchichibaha <ferchichibaha7@gmail.com>
2019-02-11 17:06:22 +0100

    add routing + fixed navbar

--- tnuniv/src/app/app-routing.module.ts
+++ tnuniv/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FindFriendsComponent } from './find-friends/find-friends.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'findfriends',component:FindFriendsComponent},
+  {path:'home',component:HomeComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const RoutingComponent=[FindFriendsComponent,HomeComponent]
--- tnuniv/src/app/app.module.ts
+++ tnuniv/src/app/app.module.ts
@@ -1,25 +1,26 @@
 import { MaterialModule } from './material';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule,RoutingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
-import { HomeComponent } from './home/home.component';
+
 import { SubjComponent } from './home/subj/subj.component';
 import { ElementComponent } from './home/subj/element/element.component';
 import { NewsComponent } from './home/news/news.component';
-import { NewsElmComponent } from './home/news/news-elm/news-elm.component'
+import { NewsElmComponent } from './home/news/news-elm/news-elm.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent,
+
     SubjComponent,
     ElementComponent,
     NewsComponent,
-    NewsElmComponent
+    NewsElmComponent,
+    RoutingComponent,
   ],
   imports: [
     BrowserModule,
